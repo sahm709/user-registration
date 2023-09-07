d08dfb4fd696cb5cea2373f25e3b3828c6c69b2a
nadhirsyah <nadhirsyah@gmail.com>
2023-09-07 12:55:49 +0700

    fix naming

--- app/components/AddUser.tsx
+++ app/components/AddUser.tsx
@@ -49,19 +49,19 @@ const AddUser = () => {
         onClick={openModal}
         className='btn btn-primary w-full'
       >
-        Add new task <AiOutlinePlus className='ml-2' size={18} />
+        Add new user <AiOutlinePlus className='ml-2' size={18} />
       </button>
 
       <Modal modalOpen={modalOpen} closeModal={closeModal}>
       <div className={`alert ${formError ? "alert-error" : "hidden"}`}>
         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-        <span>Error! Task failed successfully.</span>
+        <span>Error! register user.</span>
       </div>
         <form onSubmit={handleSubmitNewTodo}>
-          <h3 className='font-bold text-lg'>Add new task</h3>
+          <h3 className='font-bold text-lg'>Add new user</h3>
           <div className="form-control h-full">
             <label className="label">
-              <span className="label-text">Name</span>
+              <span className="label-text">Email</span>
             </label>
             <input 
               value={emailValue}
@@ -73,7 +73,7 @@ const AddUser = () => {
           </div>
           <div className="form-control h-full">
             <label className="label">
-              <span className="label-text">Job</span>
+              <span className="label-text">Password</span>
             </label>
             <input
               value={passwordValue}
